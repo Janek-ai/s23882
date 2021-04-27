b5ea55b40f5c5ac7c2505e9009910cabaf9be6e6
Janek-ai <77493996+Janek-ai@users.noreply.github.com>
2021-04-27 18:31:57 +0200

    Add files via upload

---
+++ zad3.py
@@ -0,0 +1,14 @@
+print("Aby się zalogować wpisz login i hasło")
+login=input("Wprowadz login: ")
+
+login_wlasciwy="misu"
+haslo_wlasciwe="abc123"
+if login = login_wlasciwy
+else
+	print("Taki użytkownik nie istnieje")
+print("Teraz proszę wprowadzić hasło")
+haslo=input("Wprowadz haslo: ")
+if haslo=haslo_wlasciwe
+	print("Logowanie")
+else
+	print("Wpisales błędne hasło")
